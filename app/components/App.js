2c15ab6c032ce912528ae42538c0d6fcb39a9d67
georgecode <georgeczurlanis@gmail.com>
2017-04-05 02:20:30 -0700

    Loop through objects and arrays in jsx with .map()

--- app/components/App.js
+++ app/components/App.js
@@ -9,10 +9,43 @@ function Person(name, gender){
    this.gender = gender;
 }
 
-var person = new Person("Bob", "Male");
+var people = []
+
+// var person = new Person("Bob", "Male");
+
+people.push(new Person("Bob", "Male"))
+people.push(new Person("Dana", "Female"))
+people.push(new Person("joe", "male"))
+people.push(new Person("kelly", "female"))
+
+console.log("XXXXXXXXXXXXXXXXXXXXX")
+console.log(people[0].name)
 
 // module.exports = person.name
 var fuck ="fuuuuuck";
+var hello="hello";
+
+// // testing .map (equevlent to) .each do
+// var numbers = [4, 9, 16, 5];
+
+// function myFunction(item, index) {
+//   console.log('startXXXXXXXXXXXXX ${item}')
+//     return item
+//     console.log("endXXXXXXXXXXXXX")
+
+// }
+
+// var bunchOfNumbers=numbers.map(myFunction)
+// // testing .map (equevlent to) .each do
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((numbers) =>
+  <li>{numbers}</li>
+);
+
+var listPeople = people.map((person)=>
+  <li>{person.name}</li>
+  );
+
 
 module.exports = React.createClass({
   render: function() {
@@ -20,9 +53,14 @@ module.exports = React.createClass({
 
 
       <div className="Bio">
-          <p className="Bio-text">{person.name}</p>
+          <p className="Bio-text">{people[0].name}</p>
           <p className="Bio-text">{fuck}</p>
           <p className="Bio-text">{fuck}</p>
+          <ul>{listItems}</ul>
+          <ul>{listPeople}</ul>
+
+
+          <p className="Bio-text">{2+2}</p>
       </div>
 
     )
